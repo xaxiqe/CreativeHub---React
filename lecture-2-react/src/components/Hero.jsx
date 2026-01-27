3e801bd780faba939fd6dc8e21a5d1d719b662b6
xaxiqe <vlonjat2@gmail.com>
2026-01-26 20:43:26 -0500

    work on lecture-2 layout

---
+++ lecture-2-react/src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import TextFormat from "./TextFormat";
+
+function Hero() {
+  return (
+    <>
+      <h1 style={{ textAlign: "center", marginTop: "20px" }}>
+        Creative Solutions
+      </h1>
+      <TextFormat text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas consequuntur, sapiente veniam veritatis cupiditate inventore a esse itaque harum? Omnis odio accusamus similique doloribus sed maiores quod" />
+      <TextFormat text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas consequuntur, sapiente veniam veritatis cupiditate inventore a esse itaque harum? Omnis odio accusamus similique doloribus sed maiores quod" />
+    </>
+  );
+}
+
+export default Hero;
---
+++ lecture-2-react/src/components/TextFormat.jsx
@@ -0,0 +1,15 @@
+function TextFormat({ text }) {
+  return (
+    <p
+      style={{
+        textAlign: "center",
+        marginTop: "20",
+        margin: "0 auto",
+        width: "50%",
+      }}
+    >
+      {text}
+    </p>
+  );
+}
+export default TextFormat;
