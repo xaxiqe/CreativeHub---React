dbc8833cee75b59d1df407230fe146913cb6ff82
xaxiqe <vlonjat2@gmail.com>
2026-03-13 19:41:44 -0400

    improve jokes API mini-project

--- lecture-13/REST-Api/src/components/JokesApi.jsx
+++ lecture-13/REST-Api/src/components/JokesApi.jsx
@@ -3,11 +3,15 @@ import { useEffect, useState } from "react";
 function JokesApi() {
   const [jokes, setJokes] = useState([]);
   const [favorites, setFavorites] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const fetchJokes = async () => {
+    setLoading(true);
+    await new Promise((resolve) => setTimeout(resolve, 2000));
     const res = await fetch("https://v2.jokeapi.dev/joke/Any");
     const data = await res.json();
-    setJokes((prev) => [...prev, data]);
+    setJokes((prev) => [...prev, { ...data, id: Date.now() }]);
+    setLoading(false);
   };
 
   useEffect(() => {
@@ -20,16 +24,21 @@ function JokesApi() {
     );
   };
 
+  const favJokes = jokes.filter((joke) => favorites.includes(joke.id));
+
   return (
     <div>
       <h1 className="text-red-700 text-2xl text-center">Jokes Api</h1>
+      {loading && (
+        <p className="text-center mt-3 text-gray-500">Loading joke...</p>
+      )}
 
-      {jokes.map((joke, index) => {
-        const isFavorite = favorites.includes(index);
+      {jokes.map((joke) => {
+        const isFavorite = favorites.includes(joke.id);
 
         return (
           <div
-            key={index}
+            key={joke.id}
             className="bg-gray-100 p-4 rounded-lg max-w-xl mx-auto mt-4"
           >
             {joke.type === "single" ? (
@@ -50,7 +59,7 @@ function JokesApi() {
               </button>
 
               <button
-                onClick={() => toggleFavorites(index)}
+                onClick={() => toggleFavorites(joke.id)}
                 className="px-4 py-2 bg-gray-700 text-white rounded hover:bg-gray-700/75"
               >
                 {isFavorite ? "♥️" : "🤍"}
@@ -59,11 +68,35 @@ function JokesApi() {
           </div>
         );
       })}
+
       <p className="bg-gray-100 p-4 rounded-lg max-w-xl mx-auto mt-4">
         {favorites.length > 0
           ? `Favorites:${favorites.length}`
           : "Favorites is empty"}
       </p>
+      <div className="bg-gray-100 p-4 rounded-lg max-w-xl mx-auto mt-4">
+        {favJokes.map((fav) => (
+          <div
+            key={fav.id}
+            className="bg-gray-100 p-4 border border-gray-800-600 rounded-lg max-w-xl mx-auto mt-4"
+          >
+            {fav.type === "single" ? (
+              <p>{fav.joke}</p>
+            ) : (
+              <>
+                <p className="font-semibold">{fav.setup}</p>
+                <p>{fav.delivery}</p>
+              </>
+            )}
+            <button
+              className="px-4 py-2 mt-1 bg-red-500 text-white text-sm hover:bg-gray-700/75 rounded"
+              onClick={() => toggleFavorites(fav.id)}
+            >
+              Remove Joke
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
